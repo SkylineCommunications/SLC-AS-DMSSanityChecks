8b84e644dee8eb01951cd074061aacb6b16720fb
Carolina Costa <carolina.costa@skyline.be>
2023-03-07 14:23:19 +0000

    Init solution

---
+++ SLC-AS-DMSSanityChecks_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SLC-AS-DMSSanityChecks_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("SLC-AS-DMSSanityChecks_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("FAAC5266-2362-4D07-A0E0-503BF100D41C")]
+[assembly: AssemblyVersion("1.0.0.0")]
